1fd17de461c462f4f73e87fbe2c6e140a3649d4c
Alexxus <oleoalexis@gmail.com>
2025-11-07 02:22:16 -0600

    eliminacion de clases del adaptador

--- datawarehouse/src/main/java/com/example/datawarehouse/ReportesVentasSalas/Infraestructura/IngresosEgresosPersistenciaAdaptador.java
+++
@@ -1,7 +0,0 @@
-package com.example.datawarehouse.ReportesVentasSalas.Infraestructura;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class IngresosEgresosPersistenciaAdaptador  {
-}
